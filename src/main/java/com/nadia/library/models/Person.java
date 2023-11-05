1628cc3d0b6a97cdbc92f556c7aebc3935c84a03
Nadia Lefebvre <95084122+nadialefebvre@users.noreply.github.com>
2023-11-05 18:25:24 +0100

    add Person class which User extends

---
+++ src/main/java/com/nadia/library/models/Person.java
@@ -0,0 +1,36 @@
+package com.nadia.library.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Person {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String name;
+
+  public Person() {}
+
+  public Person(String name) {
+    this.name = name;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
--- src/main/java/com/nadia/library/models/User.java
+++ src/main/java/com/nadia/library/models/User.java
@@ -1,37 +1,23 @@
 package com.nadia.library.models;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 
 @Entity
-public class User {
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-
-  private Long id;
-  private String name;
+public class User extends Person {
+  private String address; // New field for address
 
   public User() {}
 
-  public User(String name) {
-    this.name = name;
+  public User(String name, String address) {
+    super(name);
+    this.address = address;
   }
 
-  public Long getId() {
-    return id;
+  public String getAddress() {
+    return address;
   }
 
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
+  public void setAddress(String address) {
+    this.address = address;
   }
 }
